23b8edc3debcf90c693e6efb32a0d6807683edf8
y0lan <yolan@protonmail.ch>
2019-11-24 22:53:13 +0100

    Kata finished. Now doing the tests.

--- src/main/java/fr/esgi/rpg/Application.java
+++ src/main/java/fr/esgi/rpg/Application.java
@@ -5,22 +5,5 @@ import fr.esgi.rpg.jobs.Warrior;
 
 public class Application{
    public static void main(String[] args) throws Exception {
-     Faction WizardSchool = new Faction("Wizard School");
-     Faction Monster = new Faction("Monster");
-
-     Mage Healer = new Mage("Healer");
-     Warrior Yolan = new Warrior("Yolan");
-     Warrior Godzilla = new Warrior("Godzilla");
-
-     Yolan.join(WizardSchool);
-     Godzilla.join(Monster);
-
-     Yolan.attack(Godzilla);
-     Healer.join(WizardSchool);
-     Healer.join(Monster);
-     Healer.showFactions();
-     WizardSchool.addAlly(Monster);
-     Healer.heal(Godzilla);
-
    }
 }
